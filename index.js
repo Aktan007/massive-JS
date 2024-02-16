e774c5c57264f029c1f41ab7e26d08265bdf39df
Aktan Ibr. <aktan.ibraev05@mail.ru>
2024-02-16 12:10:57 +0300

    new tasks

--- index.js
+++ index.js
@@ -48,3 +48,59 @@ const doRevers = (b) => {
 };
 
 console.log(doRevers(b));
+
+//Задание 5. Сформируйте массив, состоящий из вещественных чисел. Выведите положительные элементы массива на экран.
+//Определите количество положительных элементов массива. Найти произведение отрицательных элементов массива.
+
+const nums = [3, 2, 10, -1, 0, -20, -100, 99, 34, -44, -1];
+
+for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] > 0) {
+        console.log(nums[i]);
+    } 
+};
+
+const doFilters = nums.filter((num) => num > 0)
+console.log(doFilters.length);
+
+let result = 1;
+for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] < 0) {
+        result *= nums[i]
+    }
+}
+
+console.log(result);
+
+//Задание 6. В массиве целых чисел определить число инверсий. Инверсией называется пара элементов, в которой большее число расположено слева от меньшего. 
+//Например, 24 35 29 44 8 22 4 содержит 3 инверсии.
+
+function inversionArray(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const result = []
+
+  for (let index = 0; index <= array.length; index += 2) {
+    if (!array[index + 1]) {
+      result.push(array[index])
+      break;
+    }
+
+    if (array[index] > array[index + 1]) {
+      result.push(array[index], array[index + 1])
+    } else {
+      result.push(array[index + 1], array[index])
+    }
+
+  }
+
+  return result;
+}
+
+const myArray = [3, 23, 100, 1, 30, 46, 654]
+
+console.log(inversionArray(myArray));
+
+
